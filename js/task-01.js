1c7e99a072cdc34bf9ae3c95dc807f3d902cec47
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-06 21:57:01 +0200

    1: Done

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const body = document.body;
+const categories = document.querySelectorAll("#categories > li");
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((category) =>
+  console.log(`Category: ${category.firstElementChild.textContent}
+Elements: ${category.querySelectorAll("li").length}`)
+);
