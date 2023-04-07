71c2f0126ffc3cef2bdc15973c06da88e9d7745e
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-07 15:04:14 +0200

    2: DONE

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = 
+document.querySelector("#ingredients");
+console.log(typeof list)
+
+ingredients.forEach((ingredient) => {
+ const element = document.createElement("li");
+ element.textContent = ingredient;
+ element.classList.add("item")
+ list.append(element);
+});
