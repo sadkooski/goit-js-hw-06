e895d48e7f2a805034d0ad8c2de2efab46feea3d
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-12 12:11:56 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+console.log(getRandomHexColor())
+
+const body = document.body;
+const button = document.querySelector(".change-color")
+const colorCode = document.querySelector(".color")
+// console.log(button)
+
+button.addEventListener("click", (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorCode.textContent = getRandomHexColor();
+})
+
