d84b82de3af11e7a7d31641d402f043334e9f6cf
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-16 22:52:59 +0200

    1

--- js/task-10.js
+++ js/task-10.js
@@ -1,33 +1,33 @@
-const input = document.querySelector('[type="number"]');
-// console.log(input)
-const create = document.querySelector('[data-create]');
-// console.log(create)
-const destroy = document.querySelector('[data-destroy]');
-// console.log(destroy)
-const boxes = document.querySelector("#boxes")
-// console.log(boxes)
-let amount = [];
+// const input = document.querySelector('[type="number"]');
+// // console.log(input)
+// const create = document.querySelector('[data-create]');
+// // console.log(create)
+// const destroy = document.querySelector('[data-destroy]');
+// // console.log(destroy)
+// const boxes = document.querySelector("#boxes")
+// // console.log(boxes)
+// let amount = [];
 
- input.addEventListener("input", (event) => {
-  amount.push(Number(event.currentTarget.value)); 
-  });
+//  input.addEventListener("input", (event) => {
+//   amount.push(Number(event.currentTarget.value)); 
+//   });
 
-  console.log(amount)
+//   console.log(amount)
 
-function createBoxes(amount) {
+// function createBoxes(amount) {
  
  
-for (let i = 0; i <= amount - 1; i += 1 ) {
-  console.log(document.createElement("div"))
-}
-}
-// console.log(createBoxes(amount))
-// console.log(createBoxes(amount))
+// for (let i = 0; i <= amount - 1; i += 1 ) {
+//   console.log(document.createElement("div"))
+// }
+// }
+// // console.log(createBoxes(amount))
+// // console.log(createBoxes(amount))
 
 
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
