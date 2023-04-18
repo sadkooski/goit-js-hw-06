136940b25208849decf7e15a7a31d6d385f50eaf
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-18 23:33:10 +0200

    5: fixed

--- js/task-05.js
+++ js/task-05.js
@@ -6,6 +6,6 @@ const output = document.querySelector("#name-output");
 input.addEventListener("input", (event) => { 
     if (event.currentTarget.value === "") {
         return output.textContent = "Anonymous"
-    } else
+    } 
     output.textContent = event.currentTarget.value;
 });
