a3cdfc5594ced9208d061d7163bc1fbdb33bd6f2
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-18 23:21:09 +0200

    2: fixed

--- js/task-02.js
+++ js/task-02.js
@@ -11,9 +11,15 @@ const list =
 document.querySelector("#ingredients");
 console.log(typeof list)
 
+const array = [];
+
 ingredients.forEach((ingredient) => {
  const element = document.createElement("li");
  element.textContent = ingredient;
  element.classList.add("item")
- list.append(element);
+ array.push(element)
 });
+
+ list.append(...array);
+
+
