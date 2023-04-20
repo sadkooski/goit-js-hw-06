0029d10e8186defde318b97fc9594f28eb1ffdf6
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-20 14:46:26 +0200

    10: fixed

--- js/task-10.js
+++ js/task-10.js
@@ -1,42 +1,34 @@
 const input = document.querySelector('[type="number"]');
 // console.log(input)
-const create = document.querySelector('[data-create]');
+const create = document.querySelector("[data-create]");
 // console.log(create)
-const destroy = document.querySelector('[data-destroy]');
+const destroy = document.querySelector("[data-destroy]");
 // console.log(destroy)
-const boxes = document.querySelector("#boxes")
+const boxes = document.querySelector("#boxes");
 // console.log(boxes)
 
-
 function getRandomHexColor() {
- return `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-
 function createBoxes(amount) {
-    amount = Number(this.value);
-    // console.log(amount)
- create.addEventListener('click', () => {
-    let totalSize = 20;
- for (let i = 0; i <= amount - 1; i += 1) { 
+  let totalSize = 20;
+  for (let i = 0; i <= amount - 1; i += 1) {
     totalSize += 10;
     const newDiv = document.createElement("div");
     newDiv.style.width = totalSize.toString() + "px";
     newDiv.style.height = totalSize.toString() + "px";
     newDiv.style.backgroundColor = getRandomHexColor();
     boxes.append(newDiv);
-}; 
-});
+  }
 }
-
 
 function destroyBoxes() {
-    boxes.innerHTML = '';   
+  boxes.innerHTML = "";
 }
 
-input.addEventListener('input', createBoxes);
-destroy.addEventListener('click', destroyBoxes);
-
-
+create.addEventListener("click", () => createBoxes(input.value));
+// input.addEventListener('input', createBoxes);
+destroy.addEventListener("click", destroyBoxes);
